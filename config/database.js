a4e559e8f3fdbdb74c327870e2bebd631a14c1d7
sosso21 <sofianetop21.st@gmail.com>
2021-05-26 04:30:53 +0100

    connect to db

--- config/database.js
+++ config/database.js
@@ -4,16 +4,16 @@ module.exports = ({ env }) => ({
     default: {
       connector: 'mongoose',
       settings: {
-        host: env('DATABASE_HOST', '127.0.0.1'),
-        srv: env.bool('DATABASE_SRV', false),
+        host: env('DATABASE_HOST', 'cless.azgit.mongodb.net'),
+        srv: env.bool('DATABASE_SRV', true),
         port: env.int('DATABASE_PORT', 27017),
         database: env('DATABASE_NAME', 'server'),
-        username: env('DATABASE_USERNAME' ),
+        username: env('DATABASE_USERNAME'),
         password: env('DATABASE_PASSWORD'),
       },
       options: {
         authenticationDatabase: env('AUTHENTICATION_DATABASE'),
-        ssl: env.bool('DATABASE_SSL', false),
+        ssl: env.bool('DATABASE_SSL', true),
       },
     },
   },
